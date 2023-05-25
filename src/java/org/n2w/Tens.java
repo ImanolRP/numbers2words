6f579c8920e8cc0fca5c76ad71edaa49e00e501c
ImanolRP <imanol_ramirez@hotmail.com>
2023-05-25 23:04:54 +0200

    refactor disengage of the Teens and Twenties dictionary

--- src/java/org/n2w/Tens.java
+++ src/java/org/n2w/Tens.java
@@ -2,9 +2,38 @@ package org.n2w;
 
 import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class Tens {
+
+  private static final HashMap<Long, String> teensDictionary =
+      new HashMap<Long, String>() {
+        private static final long serialVersionUID = 7198783434212592957L;
+        {
+          put(0L, "diez");
+          put(1L, "once");
+          put(2L, "doce");
+          put(3L, "trece");
+          put(4L, "catorce");
+          put(5L, "quince");
+          put(6L, "dieciséis");
+          put(7L, "diecisiete");
+          put(8L, "dieciocho");
+          put(9L, "diecinueve");
+        }
+      };
+
+  private static final HashMap<Long, String> twentiesDictionary =
+      new HashMap<Long, String>() {
+        private static final long serialVersionUID = 7198783434212592957L;
+        {
+          put(-1L, "veinti");
+          put(0L, "veinte");
+          put(2L, "veintidós");
+          put(3L, "veintitrés");
+        }
+      };
 
   public static String toWords(Long[] tokens) {
     final Long L0 = 0L;
@@ -16,19 +45,19 @@ public class Tens {
     final int TENS_IDX = 1;
 
     if (L1.equals(tokens[TENS_IDX])) {
-      return DICTIONARY.getTeens(tokens[UNIT_IDX]);
+      return teensDictionary.get(tokens[UNIT_IDX]);
     }
     if (L2.equals(tokens[TENS_IDX])) {
       if (L0.equals(tokens[UNIT_IDX])) {
-        return DICTIONARY.getTwenties(tokens[UNIT_IDX]);
+        return twentiesDictionary.get(tokens[UNIT_IDX]);
       }
       if (L2.equals(tokens[UNIT_IDX])) {
-        return DICTIONARY.getTwenties(tokens[UNIT_IDX]);
+        return twentiesDictionary.get(tokens[UNIT_IDX]);
       }
       if (L3.equals(tokens[UNIT_IDX])) {
-        return DICTIONARY.getTwenties(tokens[UNIT_IDX]);
+        return twentiesDictionary.get(tokens[UNIT_IDX]);
       }
-      return DICTIONARY.getTwenties(-1L)
+      return twentiesDictionary.get(-1L)
           .concat(Units.toWords(ArrayUtils.subarray(tokens, 0, 1)));
     }
     if (Objects.isNull(tokens[TENS_IDX])) {
