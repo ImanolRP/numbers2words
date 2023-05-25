5556178f0afa131dd08bbafeebe5a7e6387901f8
ImanolRP <imanol_ramirez@hotmail.com>
2023-05-25 19:05:42 +0200

    Green: should return "uno" when recives 1L

--- src/java/org/n2w/Numbers2Words.java
+++ src/java/org/n2w/Numbers2Words.java
@@ -5,6 +5,9 @@ public class Numbers2Words {
   private Numbers2Words() {}
 
   public static String toWords(Long number) {
+    if (1L == number) {
+      return "uno";
+    }
     return "cero";
   }
 
