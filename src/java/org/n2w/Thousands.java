6e1c9ac54872da4eb8b63b8e776686474ced077c
ImanolRP <imanol_ramirez@hotmail.com>
2023-05-27 02:57:39 +0200

    refactor of thousands for simplify conditions

--- src/java/org/n2w/Thousands.java
+++ src/java/org/n2w/Thousands.java
@@ -5,39 +5,26 @@ public class Thousands {
   private static final int BASE_IDX = 0;
   private static final int THOUSAND_IDX = 1;
 
-  private static final int UNIT_IDX = 0;
-  private static final int TENS_IDX = 1;
-  private static final int HUNDREDS_IDX = 2;
-
-  private static final String L1XXX = "mil";
+  private static final String THOUSAND = "mil";
 
   private static final String SPACE = " ";
 
   public static String get(Long number) {
     long[] tokens = tokenize(number, 3);
-    long[] tokens1 = tokenize(tokens[BASE_IDX], 1);
-    long[] tokens2 = tokenize(tokens[THOUSAND_IDX], 1);
-    if (0L == tokens2[HUNDREDS_IDX]//
-        && 0L == tokens2[TENS_IDX] //
-        && 1L == tokens2[UNIT_IDX]) {
-      if (0L == tokens1[HUNDREDS_IDX]//
-          && 0L == tokens1[TENS_IDX] //
-          && 0L == tokens1[UNIT_IDX]) {
-        return L1XXX;
+
+    if (1L == tokens[THOUSAND_IDX]) {
+      if (0L == tokens[BASE_IDX]) {
+        return THOUSAND;
       }
-      return L1XXX.concat(SPACE).concat(Hundreds.get(tokens[BASE_IDX]));
+      return THOUSAND.concat(SPACE).concat(Hundreds.get(tokens[BASE_IDX]));
     }
-    if (0L == tokens2[HUNDREDS_IDX]//
-        && 0L == tokens2[TENS_IDX] //
-        && 0L == tokens2[UNIT_IDX]) {
+    if (0L == tokens[THOUSAND_IDX]) {
       return Hundreds.get(tokens[BASE_IDX]);
     }
-    if (0L == tokens1[HUNDREDS_IDX]//
-        && 0L == tokens1[TENS_IDX] //
-        && 0L == tokens1[UNIT_IDX]) {
-      return Hundreds.get(tokens[THOUSAND_IDX]).concat(SPACE).concat(L1XXX);
+    if (0L == tokens[BASE_IDX]) {
+      return Hundreds.get(tokens[THOUSAND_IDX]).concat(SPACE).concat(THOUSAND);
     }
-    return Hundreds.get(tokens[THOUSAND_IDX]).concat(SPACE).concat(L1XXX)
+    return Hundreds.get(tokens[THOUSAND_IDX]).concat(SPACE).concat(THOUSAND)
         .concat(SPACE).concat(Hundreds.get(tokens[BASE_IDX]));
   }
 
